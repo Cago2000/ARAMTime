bf228a1c618cdd6ec29cecff62e90743e834bf69
Cago2000 <147174299+Cago2000@users.noreply.github.com>
2025-02-16 18:59:19 +0100

    adjusted file path for JSON folder

--- startup_bot.py
+++ startup_bot.py
@@ -15,7 +15,7 @@ def get_base_path():
         return os.path.dirname(os.path.abspath(__file__))
 
 base_path = get_base_path()
-bot_token_path = os.path.join(base_path, "bot_token.json")
+bot_token_path = os.path.join(base_path, "JSONs/bot_token.json")
 token = load_json(bot_token_path)
 BOT_TOKEN = token["bot_token"]
 
@@ -31,7 +31,7 @@ async def on_ready():
 
 @client.event
 async def on_presence_update(before, after):
-    params = load_json(os.path.join(base_path, "params.json"))
+    params = load_json(os.path.join(base_path, "JSONs/params.json"))
     user_ids = params["discord_user_ids"]
     your_user_id = params["your_discord_user_id"]
     game_name = params["game_name"]
@@ -39,8 +39,8 @@ async def on_presence_update(before, after):
     if user_ids is None or after.id not in user_ids:
         return
 
-    print(f'before: {before}. {before.activity}')
-    print(f'after: {after}. {after.activity}')
+    print(f'before: {before}, {before.activity}')
+    print(f'after: {after}, {after.activity}')
     print("---------------------------------------")
 
     if after.activity is None or after.activity.details != game_name:
